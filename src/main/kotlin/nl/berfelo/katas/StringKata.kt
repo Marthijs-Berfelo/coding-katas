a1fc09f245bc6fded300349a505e1145763f321c
Marthijs Berfelo <marthijs.berfelo@hybrit.org>
2023-10-05 18:50:40 +0200

    FizzBuzz :: Green :: convert to Fizz for 3

--- src/main/kotlin/nl/berfelo/katas/StringKata.kt
+++ src/main/kotlin/nl/berfelo/katas/StringKata.kt
@@ -2,5 +2,10 @@ package nl.berfelo.katas
 
 class StringKata {
 
-    fun fizzBuzz(input: Int): String = "$input"
+    fun fizzBuzz(input: Int): String =
+            if (input == 3) {
+                "Fizz"
+            } else {
+                "$input"
+            }
 }
