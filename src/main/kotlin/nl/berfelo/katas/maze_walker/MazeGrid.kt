c490afbd2ea16286f965122bed2bad972efd6348
Marthijs Berfelo <marthijs.berfelo@hybrit.org>
2023-10-06 15:17:59 +0200

    Maze walker :: Red :: fail grid without start

---
+++ src/main/kotlin/nl/berfelo/katas/maze_walker/MazeGrid.kt
@@ -0,0 +1,11 @@
+package nl.berfelo.katas.maze_walker
+
+import java.lang.IllegalArgumentException
+
+class MazeGrid(
+        val grid: Array<Array<Boolean>>,
+        val start: Point,
+        val finish: Point
+)
+
+fun parseGrid(grid: String): MazeGrid = throw IllegalArgumentException("")
---
+++ src/test/kotlin/nl/berfelo/katas/maze_walker/MazeGridTest.kt
@@ -0,0 +1,23 @@
+package nl.berfelo.katas.maze_walker
+
+import org.assertj.core.api.Assertions.assertThatThrownBy
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+
+@DisplayName("Maze grid should")
+class MazeGridTest {
+    // TODO: fail to parse grid without Finish
+    // TODO: parse grid of 1 line
+    // TODO: parse grid of 2 lines
+    // TODO: parse grid with accessible field
+    // TODO parse grid with blocked field
+
+    @Test
+    @DisplayName("fail to parse grid without Start")
+    fun rejectParsingGridWithoutStart() {
+        val grid = ""
+        assertThatThrownBy { parseGrid(grid) }
+                .isInstanceOf(IllegalArgumentException::class.java)
+                .hasMessage("Maze should have a start position set.")
+    }
+}
