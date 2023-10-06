35f4a01b55eb561a649cd72b090e417fa23613db
Marthijs Berfelo <marthijs.berfelo@hybrit.org>
2023-10-06 15:32:28 +0200

    Maze walker :: Red :: maze with start and finish

--- src/test/kotlin/nl/berfelo/katas/maze_walker/MazeGridTest.kt
+++ src/test/kotlin/nl/berfelo/katas/maze_walker/MazeGridTest.kt
@@ -1,17 +1,21 @@
 package nl.berfelo.katas.maze_walker
 
+import org.assertj.core.api.Assertions.assertThat
 import org.assertj.core.api.Assertions.assertThatThrownBy
 import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Order
 import org.junit.jupiter.api.Test
 
 @DisplayName("Maze grid should")
 class MazeGridTest {
-    // TODO: parse grid of 1 line
+    // TODO: parse grid of 1 line with start and finish accessible
+    // TODO: parse grid of 1 line with start and finish correctly positioned
     // TODO: parse grid with accessible field
     // TODO parse grid with blocked field
     // TODO: parse grid of 2 lines
 
     @Test
+    @Order(1)
     @DisplayName("fail to parse grid without Start")
     fun rejectParsingGridWithoutStart() {
         val grid = ""
@@ -21,11 +25,23 @@ class MazeGridTest {
     }
 
     @Test
+    @Order(2)
     @DisplayName("fail to parse grid without Finish")
     fun rejectParsingGridWithoutFinish() {
         val grid = "S"
         assertThatThrownBy { parseGrid(grid) }
                 .isInstanceOf(IllegalArgumentException::class.java)
                 .hasMessage("Maze should have a finish position set.")
+    }
+
+    @Test
+    @Order(3)
+    @DisplayName("parse grid of 1 line with start and finish")
+    fun acceptParsingGridWithStartAndFinish() {
+        val grid = "SF"
+        assertThat(parseGrid(grid))
+                .isInstanceOf(MazeGrid::class.java)
+                .hasFieldOrPropertyWithValue("start", true)
+                .hasFieldOrPropertyWithValue("finish", true)
     }
 }
