551117ccb4bafb1e0a45728e33d8a98bd48ca3b4
Marthijs Berfelo <marthijs.berfelo@hybrit.org>
2023-10-06 07:51:18 +0200

    FizzBuzz :: Refactor :: extension function for divisibility

--- src/main/kotlin/nl/berfelo/katas/StringKata.kt
+++ src/main/kotlin/nl/berfelo/katas/StringKata.kt
@@ -10,14 +10,14 @@ class StringKata {
     fun fizzBuzz(input: Int): String =
             ""
                     .let {
-                        if (input % 3 == 0) {
+                        if (input.isDivisibleBy(3)) {
                             it.plus("Fizz")
                         } else {
                             it
                         }
                     }
                     .let {
-                        if (input % 5 == 0) {
+                        if (input.isDivisibleBy(5)) {
                             it.plus("Buzz")
                         } else {
                             it
@@ -26,4 +26,6 @@ class StringKata {
                     .ifBlank {
                             input.toString()
                         }
+
+    private fun Int.isDivisibleBy(value: Int): Boolean = this % value == 0
 }
