c7d0b10522144e1501ce5feea511737c77b5707f
Marthijs Berfelo <marthijs.berfelo@hybrit.org>
2023-10-06 15:33:24 +0200

    Maze walker :: Refactor :: extract const

--- src/main/kotlin/nl/berfelo/katas/maze_walker/MazeGrid.kt
+++ src/main/kotlin/nl/berfelo/katas/maze_walker/MazeGrid.kt
@@ -8,11 +8,12 @@ class MazeGrid(
 )
 
 private const val startField = "S"
+private const val finishField = "F"
 
 fun parseGrid(grid: String): MazeGrid =
         if (!grid.contains(startField))
             throw IllegalArgumentException("Maze should have a start position set.")
-        else if (!grid.contains("F"))
+        else if (!grid.contains(finishField))
             throw IllegalArgumentException("Maze should have a finish position set.")
         else
             MazeGrid(start = true, finish = true)
