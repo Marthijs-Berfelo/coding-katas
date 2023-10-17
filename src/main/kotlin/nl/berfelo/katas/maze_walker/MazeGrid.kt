50f9564c045f2d56f4b216fa53bf287b002b3921
Marthijs Berfelo <marthijs.berfelo@hybrit.org>
2023-10-17 08:28:54 +0200

    Maze walker :: Refactor :: extract method

--- src/main/kotlin/nl/berfelo/katas/maze_walker/MazeGrid.kt
+++ src/main/kotlin/nl/berfelo/katas/maze_walker/MazeGrid.kt
@@ -19,7 +19,9 @@ fun parseGrid(grid: String): MazeGrid {
         throw IllegalArgumentException("Maze should have a start position set.")
     if (finish.x < 0)
         throw IllegalArgumentException("Maze should have a finish position set.")
-    return MazeGrid(start = start, finish = finish, listOf(gridRow.map { it != "#" }))
+    return MazeGrid(start = start, finish = finish, listOf(gridRow.map(::isFieldAccessible)))
 }
+
+private fun isFieldAccessible(it: String) = it != "#"
 
 private fun findField(gridRow: List<String>, fieldName: String, rowIndex: Int) = Point(gridRow.indexOf(fieldName), rowIndex)
