c8662571fe21f55778d7bd850eb69c4baec007b1
Marthijs Berfelo <marthijs.berfelo@hybrit.org>
2023-10-17 08:52:12 +0200

    Maze walker :: Green :: parse grid of multiple lines

--- src/main/kotlin/nl/berfelo/katas/maze_walker/MazeGrid.kt
+++ src/main/kotlin/nl/berfelo/katas/maze_walker/MazeGrid.kt
@@ -3,9 +3,9 @@ package nl.berfelo.katas.maze_walker
 import java.lang.IllegalArgumentException
 
 class MazeGrid(
-        val start: Point,
-        val finish: Point,
-        val maze: List<List<Boolean>>
+    val start: Point,
+    val finish: Point,
+    val maze: List<List<Boolean>>
 )
 
 private const val startField = "S"
@@ -13,17 +13,31 @@ private const val finishField = "F"
 private const val blockedField = "#"
 
 fun parseGrid(grid: String): MazeGrid {
-    val gridRow = grid.split(" ")
-    val start = findField(gridRow, startField, 0)
-    val finish = findField(gridRow, finishField, 0)
+    val gridElements = grid
+        .split("\n")
+        .map { it.split(" ") }
+        .map { line -> line.map(::isFieldAccessible) }
+    val start = findField(grid, startField)
+    val finish = findField(grid, finishField)
     if (start.x < 0)
         throw IllegalArgumentException("Maze should have a start position set.")
     if (finish.x < 0)
         throw IllegalArgumentException("Maze should have a finish position set.")
-    return MazeGrid(start = start, finish = finish, listOf(gridRow.map(::isFieldAccessible)))
+    return MazeGrid(start = start, finish = finish, gridElements)
 }
 
 private fun isFieldAccessible(it: String) = it != blockedField
+
+private fun findField(grid: String, fieldName: String): Point {
+    val gridRows = grid.split("\n")
+    val rowIndex = gridRows.indexOfFirst { it.contains(fieldName) }
+    if (rowIndex < 0) {
+        return Point(rowIndex, rowIndex)
+    }
+    return gridRows[rowIndex]
+        .split(" ")
+        .let { findField(it, fieldName, rowIndex) }
+}
 
 private fun findField(gridRow: List<String>, fieldName: String, rowIndex: Int) =
     Point(gridRow.indexOf(fieldName), rowIndex)
